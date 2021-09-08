0c41f8c018132bd8b20ab5824459c519c130440b
Riley Slater <rileymail6@gmail.com>
2021-09-08 10:29:22 -0600

    reformatting forward check to help with arc

--- project1/testCode/backtrackSolveWithLookAhead.py
+++ project1/testCode/backtrackSolveWithLookAhead.py
@@ -11,7 +11,7 @@ resets = 0
 def nextEmptySquare(puzzle):
     for i in range(0, 9):
         for j in range(0, 9):
-            if puzzle[i][j] == 0:
+            if puzzle[i, j] == 0:
                 return i, j
     return -1, -1
 
@@ -22,13 +22,13 @@ def validator(puzzle, i, j, k):
     # Checks row
     rowCheck = True
     for v in range(9):
-        if k == puzzle[i][v]:
+        if k == puzzle[i, v]:
             rowCheck = False
     if rowCheck:
         # Checks column if row passes
         columnCheck = True
         for v in range(9):
-            if k == puzzle[v][j]:
+            if k == puzzle[v, j]:
                 columnCheck = False
         if columnCheck:
             # check 3x3 sections if row and column pass
@@ -36,7 +36,7 @@ def validator(puzzle, i, j, k):
             topX, topY = 3 * (i // 3), 3 * (j // 3)
             for x in range(topX, topX + 3):
                 for y in range(topY, topY + 3):
-                    if puzzle[x][y] == k:
+                    if puzzle[x, y] == k:
                         return False
             return True
     return False
@@ -45,7 +45,7 @@ def validator(puzzle, i, j, k):
 def forwardChecking(puzzle, i, j, k):
     global sections
 
-    puzzle[i][j] = k
+    puzzle[i, j] = k
     check = [(i, j, k)]
     sections = [[0, 3, 0, 3],
                 [3, 6, 0, 3],
@@ -65,13 +65,13 @@ def forwardChecking(puzzle, i, j, k):
         # find the missing cells in the sections
         for x in range(sections[v][0], sections[v][1]):
             for y in range(sections[v][2], sections[v][3]):
-                if puzzle[x][y] != 0:
-                    domain.remove(puzzle[x][y])
+                if puzzle[x, y] != 0:
+                    domain.remove(puzzle[x, y])
 
         # attach the domain to each empty cell
         for x in range(sections[v][0], sections[v][1]):
             for y in range(sections[v][2], sections[v][3]):
-                if puzzle[x][y] == 0:
+                if puzzle[x, y] == 0:
                     tracking.append([x, y, domain.copy()])
 
         for n in range(len(tracking)):
@@ -80,28 +80,28 @@ def forwardChecking(puzzle, i, j, k):
             # remove the elements in row n
             rowCheck = []
             for x in range(9):
-                rowCheck.append(puzzle[trackingItem[0]][x])
+                rowCheck.append(puzzle[trackingItem[0], x])
             remaining = trackingItem[2].difference(rowCheck)
 
             # remove the elements in col n
             colCheck = []
             for y in range(9):
-                colCheck.append(puzzle[y][trackingItem[1]])
+                colCheck.append(puzzle[y, trackingItem[1]])
             remaining = remaining.difference(colCheck)
 
             # check for duplicates of the domain
             if len(remaining) == 1:
                 value = remaining.pop()
                 if validator(puzzle, trackingItem[0], trackingItem[1], value):
-                    puzzle[trackingItem[0]][trackingItem[1]] = value
+                    puzzle[trackingItem[0], trackingItem[1]] = value
                     check.append((trackingItem[0], trackingItem[1], value))
-    return check
+    return check, tracking
 
 
 # Undo the forward checking algorithm
 def resetForwardChecking(puzzle, check):
     for i in range(len(check)):
-        puzzle[check[i][0]][check[i][1]] = 0
+        puzzle[check[i][0], check[i][1]] = 0
     return
 
 
@@ -119,7 +119,7 @@ def recursiveBacktrackSolve(puzzle, i=0, j=0):
         if validator(puzzle, i, j, k):
             puzzle[i][j] = k
 
-            check = forwardChecking(puzzle, i, j, k)
+            check, domains = forwardChecking(puzzle, i, j, k)
 
             if recursiveBacktrackSolve(puzzle, i, j):
                 return True
