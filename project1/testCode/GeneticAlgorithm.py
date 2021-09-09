cdab61626b1c7a630e0051c09f6712a1f510ed4f
Cooper Strahan <cooperstrahan@gmail.com>
2021-09-09 14:18:01 -0600

    main.py

---
+++ project1/testCode/GeneticAlgorithm.py
@@ -0,0 +1,13 @@
+
+from project1.testCode.LocalSearch import LocalSearch
+
+import LocalSearch
+
+class GeneticAlgorithm(LocalSearch):
+    
+    def __init__(self) -> None:
+        super().__init__()
+
+    
+    def solve():
+        return
--- project1/testCode/main.py
+++ project1/testCode/main.py
@@ -1,17 +1,8 @@
 import puzzleImporter as puz
 import backtrackSolve as bt
-import backtrackSolveWithLookAhead as btfc
 
-test1 = puz.importPuzzle("..\\testPuzzles\\Easy-P2.csv")
-
-
+test1 = puz.importPuzzle("..\\testPuzzles\\Easy-P1.csv")
+bt.displayPuzzle(test1)
 bt.recursiveBacktrackSolve(test1)
 print()
 bt.displayPuzzle(test1)
-print()
-
-##btfc.recursiveBacktrackSolve(test1)
-##print()
-##btfc.displayPuzzle(test1)
-##print()
-
